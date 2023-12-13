a85ced7baf7056e12b46c250e4ed004fd4278ea4
Nikhil Potdar <nikhilpotdar1415@gmail.com>
2023-12-14 00:16:52 +0530

    Initial commit

---
+++ Git.js
@@ -0,0 +1,20 @@
+// Importing the 'http' module
+const http = require('http');
+
+// Creating a server
+const server = http.createServer((req, res) => {
+  // Setting the response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Sending the response
+  res.end('Hello, World!\n');
+});
+
+// Setting the port and IP address to listen on
+const port = 3000;
+const host = '127.0.0.1';
+
+// Starting the server
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
